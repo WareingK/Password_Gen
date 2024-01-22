b485e1d75383d6970fd710f5333cedee99b0e42d
Kristian Wareing <l1gw@outlook.com>
2024-01-21 18:47:49 -0800

    Added additional options to choose from password

--- Develop/script.js
+++ Develop/script.js
@@ -19,6 +19,38 @@ function generatePassword() {
     alert('Please enter a valid password length between 8 and 128 characters.');
     return;
   }
+  // Ask about extra criteria for password
+  var includeLowercase = confirm("Include lowercase Characters?");
+  var includeUppercase = confirm('Include uppercase characters?');
+  var includeNumeric = confirm('Include numeric characters?');
+  var includeSpecialChars = confirm('Include special characters?');
+  // Confirm that at least one criteria is selected
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecialChars) {
+    alert('Please select at least one criteria');
+    return;
+  }
+  //Added the characters to the criteria
+  var lowercaseChars = 'abcdefghijklmnopqrstuvwyz';
+  var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numbericChars = '0123456789';
+  var specialChars = '!@#$%^&*()_+{}[];:,.<>?~';
+  // Add the characters to the criteria questions
+  var allChars = '';
+  if (includeLowercase) allChars += lowercaseChars;
+  if (includeUppercase) allChars += uppercaseChars;
+  if (includeNumeric) allChars += numbericChars;
+  if (includeSpecialChars) allChars += specialChars;
+  //Finally generate the password
+  var generatedPassword = '';
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * allChars.length);
+    generatePassword += allChars.charAt(randomIndex);
+  }
+  return generatedPassword;
 }
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Show the password
