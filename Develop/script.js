0c10279c1699315d2a9bdfebf728c09408535432
Kristian Wareing <l1gw@outlook.com>
2024-01-21 18:23:59 -0800

    Added alert for password length

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,16 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword() {
+  // Prompt for the criteria of user
+  var passwordLength = prompt("Enter the length of the password between 8 and 128 characters.");
+  passwordLength = parseInt(passwordLength);
 
+  // Make sure the password length is correct
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert('Please enter a valid password length between 8 and 128 characters.');
+    return;
+  }
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
